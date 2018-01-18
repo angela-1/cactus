d1aae74e6ea84c799df0b2973578633424a74dfa
Angela-1 <ruoshui_engr@163.com>
2018-01-18 17:39:43 +0800

    add make toc func.

--- cactus/Toc.cs
+++ cactus/Toc.cs
@@ -17,11 +17,22 @@ namespace cactus
             Paragraphs pars = thisDoc.Paragraphs;
             int parCount = thisDoc.Paragraphs.Count;
             Selection cursor = Globals.ThisAddIn.Application.Selection;
-           
+
+            cursor.ParagraphFormat.SpaceBeforeAuto = 0;
+            cursor.ParagraphFormat.SpaceAfterAuto = 0;
+            cursor.ParagraphFormat.LeftIndent = 0;
+            cursor.ParagraphFormat.FirstLineIndent = 0;
+            cursor.ParagraphFormat.CharacterUnitFirstLineIndent = 0;
+            cursor.ParagraphFormat.WordWrap = 0;
+
+            cursor.ParagraphFormat.FirstLineIndent = 0;
+
             cursor.ParagraphFormat.TabStops.ClearAll();
             cursor.ParagraphFormat.TabStops.Add(Position: app.CentimetersToPoints(14.2F),
                 Alignment: WdHorizontalLineAlignment.wdHorizontalLineAlignLeft, 
                 Leader: WdTabLeader.wdTabLeaderDots);
+
+          
 
             System.Windows.Forms.MessageBox.Show("目录制作完成。");
         }
