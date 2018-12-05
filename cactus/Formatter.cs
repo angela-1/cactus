36fa4bf2667982504f81ce31ea26759c9ba1ec06
Angela-1 <ruoshui_engr@163.com>
2018-12-05 11:32:18 +0800

    修改程序逻辑。

--- cactus/Formatter.cs
+++ cactus/Formatter.cs
@@ -14,9 +14,12 @@ namespace cactus
             Paragraphs pars = thisDoc.Paragraphs;
             int parCount = thisDoc.Paragraphs.Count;
             Selection cursor = app.Selection;
-            cursor.ClearFormatting();
 
+            // 自动编号的标题变成文字
             thisDoc.Content.ListFormat.ConvertNumbersToText();
+
+            // 清除所有格式变为默认，为正则检测做准备
+            cursor.ClearFormatting();
 
             // 判断是否选择了段落
             if (cursor.Paragraphs.Count <= 1)
@@ -26,7 +29,6 @@ namespace cactus
             }
 
             // 获取选中的要修改样式的开头段落和结尾段落
-            //
             int startFormatPar = this._get_format_start(cursor, pars);
             int endFormatPar = this._get_format_end(cursor, pars);
 
@@ -38,25 +40,22 @@ namespace cactus
             Regex reLevel2 = new Regex("^（[一二三四五六七八九十]+）");
             //Regex reLevel3 = new Regex("^[0-9]. ");
 
-            Boolean level1FontLock = true;
-            Boolean level2FontLock = true;
+            bool level1FontLock = true;
+            bool level2FontLock = true;
+
+            Paragraph par;
 
             for (int i = startFormatPar; i < endFormatPar; i++)
             {
-                Paragraph par = pars[i];
+                par = pars[i];
                 par.Range.Text = par.Range.Text.Replace("　", "").Replace(" ", "");
-            }
-
-            for (int i = startFormatPar; i < endFormatPar; i++)
-            {
-                Paragraph par = pars[i];
-                string lineStart = par.Range.Text;
-                if (reLevel1.IsMatch(lineStart, 0) && lineStart.Length > 24)
+                string onePar = par.Range.Text;
+                if (reLevel1.IsMatch(onePar, 0) && onePar.Length > 24)
                 {
                     level1FontLock = false;
                     break;
                 }
-                if (reLevel2.IsMatch(lineStart) && lineStart.Length > 24)
+                if (reLevel2.IsMatch(onePar) && onePar.Length > 24)
                 {
                     level2FontLock = false;
                     break;
@@ -66,7 +65,7 @@ namespace cactus
             // 正则检测每段开头对应修改样式
             for (int i = startFormatPar; i < endFormatPar; i++)
             {
-                Paragraph par = pars[i];
+                par = pars[i];
                 // 需要格式化的从开头到结尾都是首行缩进2字符，三号字，固定行距28磅
                 par.LeftIndent = app.CentimetersToPoints(0);
                 par.RightIndent = app.CentimetersToPoints(0);
@@ -84,10 +83,10 @@ namespace cactus
                 par.LineUnitBefore = 0;
                 par.LineUnitAfter = 0;
 
-                string lineStart = par.Range.Text;
+                string onePar = par.Range.Text;
 
                 // 一级标题黑体
-                if (level1FontLock && reLevel1.IsMatch(lineStart))
+                if (level1FontLock && reLevel1.IsMatch(onePar))
                 {
                     par.Range.Font.Name = "黑体";
                     par.Range.Font.NameAscii = "Tmimes New Roman";
@@ -95,7 +94,7 @@ namespace cactus
                     par.Range.ParagraphFormat.KeepWithNext = -1;
                 }
                 // 二级标题楷体
-                else if (level2FontLock && reLevel2.IsMatch(lineStart))
+                else if (level2FontLock && reLevel2.IsMatch(onePar))
                 {
                     par.Range.Font.Name = "方正楷体_GBK";
                     par.Range.Font.NameAscii = "Times New Roman";
